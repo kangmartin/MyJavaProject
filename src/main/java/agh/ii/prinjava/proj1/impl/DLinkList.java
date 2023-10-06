25f7a737958d08cfc5835114426c3f4b1b9efe5c
Martin Kang <martin.kang@efrei.net>
2023-10-06 16:12:14 +0200

    Add methods and unit tests

--- src/main/java/agh/ii/prinjava/proj1/impl/DLinkList.java
+++ src/main/java/agh/ii/prinjava/proj1/impl/DLinkList.java
@@ -1,10 +1,59 @@
 package agh.ii.prinjava.proj1.impl;
 
 public class DLinkList<E> {
-    // ...
+    private Node<E> first;
+    private Node<E> last;
+
     private static class Node<T> {
         T elem;
         Node<T> next;
         Node<T> prev;
+    }
+
+    public void addFirst(E e) {
+        Node<E> newNode = new Node<>();
+        newNode.elem = e;
+        newNode.next = first;
+        if (first != null) first.prev = newNode;
+        else last = newNode;
+        first = newNode;
+    }
+
+    public void addLast(E e) {
+        Node<E> newNode = new Node<>();
+        newNode.elem = e;
+        newNode.prev = last;
+        if (last != null) last.next = newNode;
+        else first = newNode;
+        last = newNode;
+    }
+
+    public E removeFirst() {
+        if (first == null) throw new RuntimeException("Cannot remove from an empty list");
+        E elem = first.elem;
+        first = first.next;
+        if (first != null) first.prev = null;
+        else last = null;
+        return elem;
+    }
+
+    public E removeLast() {
+        if (last == null) throw new RuntimeException("Cannot remove from an empty list");
+        E elem = last.elem;
+        last = last.prev;
+        if (last != null) last.next = null;
+        else first = null;
+        return elem;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<E> current = first;
+        while (current != null) {
+            sb.append(current.elem).append(" ");
+            current = current.next;
+        }
+        return sb.toString().trim();
     }
 }
--- src/test/java/agh/ii/prinjava/proj1/impl/DLinkListTest.java
+++ src/test/java/agh/ii/prinjava/proj1/impl/DLinkListTest.java
@@ -2,17 +2,62 @@ package agh.ii.prinjava.proj1.impl;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DLinkListTest {
-    DLinkList<Integer> dLinkList = new DLinkList<>();
+    DLinkList<Integer> dLinkList;
 
     @BeforeEach
     void setUp() {
+        dLinkList = new DLinkList<>();
     }
 
     @AfterEach
     void tearDown() {
+        dLinkList = null;
+    }
+
+    @Test
+    void testAddFirst() {
+        System.out.println("Test addFirst method:");
+        dLinkList.addFirst(1);
+        System.out.println("Add first: 1 -> " + dLinkList.toString());
+        dLinkList.addFirst(2);
+        System.out.println("Add first: 2 -> " + dLinkList.toString());
+        assertEquals("2 1", dLinkList.toString());
+    }
+
+    @Test
+    void testAddLast() {
+        System.out.println("Test addLast method:");
+        dLinkList.addLast(1);
+        System.out.println("Add last: 1 -> " + dLinkList.toString());
+        dLinkList.addLast(2);
+        System.out.println("Add last: 2 -> " + dLinkList.toString());
+        assertEquals("1 2", dLinkList.toString());
+    }
+
+    @Test
+    void testRemoveFirst() {
+        System.out.println("Test removeFirst method:");
+        dLinkList.addFirst(1);
+        dLinkList.addFirst(2);
+        System.out.println("List after adding elements: " + dLinkList.toString());
+        int removedElement = dLinkList.removeFirst();
+        System.out.println("Removed first element: " + removedElement + " -> " + dLinkList.toString());
+        assertEquals(1, dLinkList.removeFirst());
+    }
+
+    @Test
+    void testRemoveLast() {
+        System.out.println("Test removeLast method:");
+        dLinkList.addLast(1);
+        dLinkList.addLast(2);
+        System.out.println("List after adding elements: " + dLinkList.toString());
+        int removedElement = dLinkList.removeLast();
+        System.out.println("Removed last element: " + removedElement + " -> " + dLinkList.toString());
+        assertEquals(1, dLinkList.removeLast());
     }
 }
