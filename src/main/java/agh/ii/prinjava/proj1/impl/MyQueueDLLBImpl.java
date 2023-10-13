68ee31b269f3ba79bc28fa7f23e1e52d0f829bc7
Martin Kang <martin.kang@efrei.net>
2023-10-13 13:13:48 +0200

    Implementation of class MyQueueDLLBImpl

--- src/main/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImpl.java
+++ src/main/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImpl.java
@@ -3,25 +3,44 @@ package agh.ii.prinjava.proj1.impl;
 import agh.ii.prinjava.proj1.MyQueue;
 
 public class MyQueueDLLBImpl<E> implements MyQueue<E> {
-    private DLinkList<E> elems;
 
+    private final DLinkList<E> elems;
+    private int size = 0;
+
+    /** Constructor to initialize the queue. */
+    public MyQueueDLLBImpl() {
+        this.elems = new DLinkList<>();
+    }
+
+    /** Enqueue an element to the end of the queue. */
     @Override
     public void enqueue(E x) {
-        throw new IllegalStateException("To be implemented");
+
+        elems.addLast(x);
+        size++;
     }
 
+    /** Dequeue and return the first element from the queue. */
     @Override
     public E dequeue() {
-        throw new IllegalStateException("To be implemented");
+
+        size--;
+        return elems.removeFirst();
     }
 
+    /** Return the number of elements in the queue. */
     @Override
     public int numOfElems() {
-        return 0;
+
+        return size;
     }
 
+    /** Peek and return the first element without removing it. */
     @Override
     public E peek() {
-        throw new IllegalStateException("To be implemented");
+
+        E firstElem = elems.removeFirst();
+        elems.addFirst(firstElem);
+        return firstElem;
     }
 }
--- src/test/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImplTest.java
+++ src/test/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImplTest.java
@@ -1,19 +1,46 @@
 package agh.ii.prinjava.proj1.impl;
 
 import agh.ii.prinjava.proj1.MyQueue;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyQueueDLLBImplTest {
     MyQueue<Integer> queueOfInts = MyQueue.create();
-
-    @BeforeEach
-    void setUp() {
+    /** Test the enqueue method. */
+    @Test
+    void testEnqueue() {
+        queueOfInts.enqueue(1);
+        assertEquals(1, queueOfInts.numOfElems());
+        assertEquals(1, queueOfInts.peek());
     }
 
-    @AfterEach
-    void tearDown() {
+    /** Test the dequeue method. */
+    @Test
+    void testDequeue() {
+        queueOfInts.enqueue(1);
+        queueOfInts.enqueue(2);
+        assertEquals(1, queueOfInts.dequeue());
+        assertEquals(1, queueOfInts.numOfElems());
+    }
+
+    /** Test the numOfElems method. */
+    @Test
+    void testNumOfElems() {
+        assertEquals(0, queueOfInts.numOfElems());
+        queueOfInts.enqueue(1);
+        queueOfInts.enqueue(2);
+        queueOfInts.enqueue(3);
+        queueOfInts.enqueue(4);
+        assertEquals(4, queueOfInts.numOfElems());
+    }
+
+    /** Test the peek method. */
+    @Test
+    void testPeek() {
+        queueOfInts.enqueue(1);
+        assertEquals(1, queueOfInts.peek());
+        queueOfInts.enqueue(2);
+        assertEquals(1, queueOfInts.peek());
     }
 }
