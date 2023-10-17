d2501a410709c99eaa8696d4833c2c981e2d67d4
Martin Kang <martin.kang@efrei.net>
2023-10-17 18:26:20 +0200

    Improve Javadoc comments

--- src/test/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImplTest.java
+++ src/test/java/agh/ii/prinjava/proj1/impl/MyQueueDLLBImplTest.java
@@ -1,11 +1,13 @@
 package agh.ii.prinjava.proj1.impl;
 
 import agh.ii.prinjava.proj1.MyQueue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyQueueDLLBImplTest {
+
     MyQueue<Integer> queueOfInts = MyQueue.create();
 
     /**
